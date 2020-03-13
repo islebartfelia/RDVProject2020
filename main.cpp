d6881ba36892b89d672a87730a4a3efd49015dfc
TRABELSI ILEF <43601911+islebartfelia@users.noreply.github.com>
2020-03-13 10:54:34 +0100

    Drawing up all triangles

--- main.cpp
+++ main.cpp
@@ -9,50 +9,18 @@
 
 
 
-std::vector< std::vector<float> > read(std::string Filename, std::string w) {
-
-    std::ifstream file(Filename.c_str());
-    std::vector< std::vector<float> > coord;
-    std::vector<float> p;
-    std::string line;
-
-
-    if (file) {
-
-        while (file >> line) {
-
-            while (line == w) {
-
-                file >> line;
-                p.push_back(atof(line.c_str()));
-                file >> line;
-                p.push_back(atof(line.c_str()));
-                file >> line;
-                file >> line;
-                coord.push_back(p);
-
-            }
-            //std::cout << "okk" << std::endl;
-        }
-
-    }
-    else {
-        std::cout << "File not found " << std::endl;
-    }
-    return coord;
-}
 
 
 void line(int x0, int y0, int x1, int y1, Image * image, Pixel pixel) { //with Bresenham methode
     bool steep = false;
     if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-        std::cout << "here 1 " << std::endl;
+       
         std::swap(x0, y0);
         std::swap(x1, y1);
         steep = true;
     }
     if (x0 > x1) {
-        std::cout << "here 2 " << std::endl;
+     
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
@@ -88,6 +56,43 @@ void triangle(std::vector<float> p, std::vector<float> p1, std::vector<float> p2
     line(p2[0], p2[1], p[0], p[1], image, pixel);
 }
 
+std::vector< std::vector<float> > read(std::string Filename, std::string w  ) {
+
+    std::ifstream file(Filename.c_str());
+    std::vector< std::vector<float> > coord;
+    std::vector<float> p;
+    std::string line;
+
+
+    if (file) {
+
+        while (file >> line) {
+
+            while (line == w) {
+
+                file >> line;
+                p.push_back((atof(line.c_str())*100) +400); //x 
+
+                file >> line;
+                p.push_back((atof(line.c_str() )* 100) + 400);//y
+
+                file >> line;
+                //  p.push_back(atof(line.c_str()) );//z
+                file >> line;
+                coord.push_back(p);
+
+              
+            }
+        }
+
+    }
+    else {
+        std::cout << "File not found " << std::endl;
+    }
+    return coord;
+}
+
+
 int main() {
 
     int width = 800; int height = 800;
@@ -101,42 +106,56 @@ int main() {
          for (height = 0; height < 800; height++)
          {
              Pixel p;
-             p.r = 0;
-             p.g =0;
-             p.b =200;
+             p.r = 102;
+             p.g =153;
+             p.b =153;
 
              Image::setPixel(I, width, height, p);
          }
      }
-    //image.sauver
+     //image.sauver
      Image::sauver(I, "test.PPM");
-    std::vector< std::vector<float> > coord = read("diablo3_pose.obj", "v");
+     std::vector< std::vector<float> > coord = read("diablo3_pose.obj", "v");
+   
 
+     std::vector<float> pt;
+     std::vector<float> p1;
+     std::vector<float> p2;
+     std::vector<float> p3;
 
-   // Tester triangle
-    std::vector<float> p;
-    std::vector<float> p1;
-    std::vector<float> p2;
-    p.push_back(100);
-    p.push_back(500);
-    p1.push_back(100);
-    p1.push_back(200);
-    p2.push_back(300);
-    p2.push_back(120);      
-            Pixel pi;
-            pi.r = 0;
-            pi.g = 200;
-            pi.b = 0;
-    triangle(p, p1, p2,I, pi);
-    //image.sauver
-    Image::sauver(I, "test.PPM");
+     int i = 1;
+     for (auto point : coord)
+     {  
     
+             pt = point;
+             
+            
         
+     }
+     Pixel pi;
+     pi.r = 255;
+     pi.g = 0;
+     pi.b = 0;
+
     
-  
+     for (i = 0; i < pt.size()-5; i++) {
+         p1.push_back(pt[i]);
+         p1.push_back(pt[i+1] );
+         p2.push_back(pt[i+2] );
+         p2.push_back(pt[i+3] );
+         p3.push_back(pt[i+4] );
+         p3.push_back(pt[i+5] );
 
-
-
+         
+         triangle(p1, p2, p3, I, pi);
+         p1.clear();
+         p2.clear();
+         p3.clear();
+        
+     }
+     //image.sauver
+     Image::sauver(I, "triangle.PPM");
+ 
     return 0;
 
 }
